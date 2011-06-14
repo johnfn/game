b2259b1d61d36384864ac695b688f4743c776924
johnfn <johnfn@gmail.com>
2011-06-14 00:45:21 -0400

    Collision detection.

--- src/game/core.clj
+++ src/game/core.clj
@@ -1,5 +1,8 @@
+;(defn test-fn [& f])
+
 (ns game.test.core
-  (:require [src.game.test]))
+  (:require [src.game.test])
+)
 
 ;(set! *warn-on-reflection* true)
 
@@ -18,6 +21,8 @@
 
 (def x (atom 0))
 (def y (atom 0))
+
+(def running (atom true))
 
 (def keys-down (map (fn [x] (atom 0)) (range 255)))
 
@@ -46,14 +51,13 @@
   (when *DEBUG*
     `(do ~@body)))
 
+(defmacro when [test & body] 
+  (list 'if test `(do ~@body) 0))
+
 (defn get-keys-down [keys-down]
   ;Map all 1s to their position in the list, then filter out all 0s. Yay functional!
   (filter (fn [x] (not (= 0 x))) (for [x (range 255)] (if (= @(nth keys-down x) 0) 0 x))))
 
-"87 W
- 65 A
- 83 S
- 68 D"
 (def panel
   (proxy [JPanel KeyListener] []
     (getPreferredSize [] (Dimension. 100 100))
@@ -92,21 +96,80 @@
 (test-fn rel-to-abs
          '(0 0) '(0 30))
 
+(defn in? [list item]
+  (some #(= item %) list))
+
+(test-fn in?
+         '((1 2 3 4 6) 1) true
+         '((1 2 3 4 6) 2) true
+         '((1 2 3 4 6) 5) nil
+         )
+
+(defn +list [a b]
+  (for [x (range (count a))]
+    (+ (nth a x) (nth b x))))
+
+(test-fn +list
+         '((1 2 3) (2 3 4)) '(3 5 7)
+         '((1) (5)) '(6)
+         )
+
+
+"87 W
+ 65 A
+ 83 S
+ 68 D"
+(defn get-delta [keys-down]
+  (let [W 87
+        A 65
+        S 83
+        D 68
+        dy (+ (when (in? keys-down W) -1)
+              (when (in? keys-down S)  1))
+        dx (+ (when (in? keys-down D)  1)
+              (when (in? keys-down A) -1))]
+    (list dx dy)))
+
+(defn valid-position? [pos]
+  (let [[pos-x pos-y] pos]
+    (not (= (get-tile-data pos-x pos-y) \1))))
+
+;TODO: Actual implementation of this fn.
+(defn set-whats-necessary [pos]
+  (let [[new-x new-y] pos]
+    (reset! x new-x)
+    (reset! y new-y)))
+
+(defn game-step [keys-down]
+  (let [delta (get-delta keys-down)
+        new-pos (+list delta (list @x @y))]
+    (when (valid-position? new-pos)
+      (set-whats-necessary new-pos))))
+
+;
+; Graphics
+;
+
+
 (defn draw-tile [gfx x y type]
+  "Draws an individual tile."
   (let [[tile-x tile-y] (rel-to-abs x y)]
     (.setColor gfx type)
     (.fillRect gfx tile-x tile-y *tile-width* *tile-width*)))
 
 (defn draw-tiles [gfx]
+  "Draw all tiles."
   (doseq [x (range *map-width*)
           y (range *map-width*)]
     (draw-tile gfx x y (get-tile-type (get-tile-data x y)))))
 
 (defn draw-characters [gfx window]
+  "Draw all characters."
   (doseq [key (keys *characters*)]
     (draw-tile gfx @x @y (get-tile-type \c))))
 
 (defn draw-state [gfx window]
+  "The top level drawing function."
   (let [image (.createImage window *width* *height*)
         off-gfx (.createGraphics image)]
     (draw-tiles off-gfx)
@@ -114,29 +177,28 @@
     (.drawImage gfx image 0 0 window)))
 
 (defn make-frame [w h]
-  (def frame (java.awt.Frame.))
+  (def frame (javax.swing.JFrame.))
 
   (.setVisible frame true)
   (.setSize frame w h)
   (.add frame panel)
   (.pack frame)
-  ;(.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
+  (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
   frame)
-
-(defn game-step [keys-down]
-  )
 
 (defn game-loop [frame]
   (let [gfx (.getGraphics frame)]
     (Thread/sleep *delay*)
     (game-step (get-keys-down keys-down))
     (draw-state gfx frame)
-    (recur frame)))
+      (recur frame)))
 
 (defn run-tests [x]
   (println "Testing..."))
 
 (defn main []
-  (game-loop (make-frame *width* *height*)))
+  ;Run game-loop in a separate thread so that repl works.
+  (do (game-loop (make-frame *width* *height*))))
+
 
 (main)
--- src/game/test.clj
+++ src/game/test.clj
@@ -1,18 +1,26 @@
+;(ns game-tests
+;  (:require clojure.contrib.str-utils))
+
 (defn fn-name [fn]
   (let [repr (str fn)]
-    (nth (nth (re-seq #"\$([a-zA-Z0-9_]*)@" repr) 0) 1)))
+;    (clojure.contrib.str-utils/replace-first-str "_QMARK_" "?"
+      (nth (nth (re-seq #"\$([a-zA-Z0-9_]*)@" repr) 0) 1))) ;)
 
 (defn test-fn [fn & tests]
   (println "Testing" (fn-name fn))
   (defn test-body 
-      ([passed total] 
-        (println "Passed" passed "of" total)
-        (when (= passed total)
-          (println "Everything passed! Hooray!\n")))
-      ([passed total & more]
-        (let [passed? (= (apply fn (first more)) (second more))
-              new-passed (if passed? (+ passed 1) passed)]
-          (when-not passed? (print "-"))
-          (apply test-body (list* new-passed (+ total 1) (next (next more)))))))
+    ; run a test
+    ([passed total & more]
+      (let [passed? (= (apply fn (first more)) (second more))
+            new-passed (if passed? (+ passed 1) passed)]
+        (when-not passed? (print "-"))
+        (apply test-body (list* new-passed (+ total 1) (next (next more))))))
+
+    ; done; print out statistics
+    ([passed total] 
+      (println "Passed" passed "of" total)
+      (when (= passed total)
+        (println "Everything passed! Hooray!\n"))))
+
        
   (apply test-body (list* 0 0 tests)))
