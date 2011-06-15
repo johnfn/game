ab3e5459f7973e32a924d6a851ef6024bc1294ac
johnfn <johnfn@gmail.com>
2011-06-15 00:37:28 -0400

    No more atoms for x/y. Instead, we create and pass around a state.

--- src/game/core.clj
+++ src/game/core.clj
@@ -22,9 +22,6 @@
 (def *map-width* 30)
 (def *abs-map-width* (* *map-width* *tile-width*))
 
-(def x (atom 30))
-(def y (atom 30))
-
 (def keys-down (map (fn [x] (atom 0)) (range 255)))
 
 (def simple-map-2
@@ -115,10 +112,10 @@
 (defn +list [& lists]
   (let [len (count (nth lists 0))]
     (map 
-      (fn [x] ;for each position
+      (fn [pos] ;for each position
         (apply +  ;sum
                (map (fn [list] ; each element of each list
-                      (nth list x)) ;that's at that position
+                      (nth list pos)) ;that's at that position
                     lists))) 
       (range len))))
 
@@ -154,10 +151,11 @@
          '(1 1) \0
          '(1 2) \1)
 
-(defn abs-to-abs [x-pos y-pos]
+;TODO stopped here
+(defn abs-to-abs [state x-pos y-pos]
   "Take an absolute position and put it in the correct place. I really need to think of a better fn name."
-  (let [x-offs @x
-        y-offs @y]
+  (let [x-offs (get state :x)
+        y-offs (get state :y)]
     (+list
       [(+ (- x-offs) 50) (+ (- y-offs) 50)] ;adjust for moving map
       [0 30] ;adjust for title bar
@@ -173,11 +171,11 @@
     [(int (/ i *tile-width*))
      (int (/ j *tile-width*))]))
 
-(defn rel-to-pos [x y]
-  (apply abs-to-abs (rel-to-abs x y)))
+(defn rel-to-pos [state x y]
+  (apply abs-to-abs state (rel-to-abs x y)))
 
-(test-fn rel-to-pos
-         '(0 0) '(0 30))
+;(test-fn rel-to-pos
+;         '(0 0) '(0 30))
 
 (defn in? [list item]
   (some #(= item %) list))
@@ -218,10 +216,13 @@
          positions))))
 
 ;TODO: Actual implementation of this fn.
-(defn add-whats-necessary [pos]
-  (let [[new-x new-y] pos]
-    (reset! x (+ @x new-x))
-    (reset! y (+ @y new-y))))
+(defn add-whats-necessary [state pos]
+  (let [old-x (get state :x)
+        old-y (get state :y)
+        [dx dy] pos
+        new-x (+ old-x dx)
+        new-y (+ old-y dy)]
+    (assoc state :x new-x :y new-y)))
 
 ;This is just an idea (untested code)
 (defmacro try-all [x y & body]
@@ -235,42 +236,48 @@
   "Changes the map if necessary (going offscreen on one side goes on the other side of another). 
   Returns game state."
 
+  ;TODO: This function is getting pretty gnarly.
+  
   (let [[dx dy] (get-delta keys-down)
-        x (+ @x dx)
-        y (+ @y dy)
+        x (+ (get state :x) dx)
+        y (+ (get state :y) dy)
         cur-map (get state :cur-map)]
       (cond 
         (< x 0)
           (do
-            (add-whats-necessary [(- *abs-map-width* 16) 0]) 
-            (assoc state :cur-map simple-map-2))
+            (let [state (add-whats-necessary state [(- *abs-map-width* 16) 0])]
+              (assoc state :cur-map simple-map-2)))
         (> (+ x *player-width*) *abs-map-width*)
           (do
-            (add-whats-necessary [(- (- *abs-map-width* 16)) 0]) 
-            (assoc state :cur-map simple-map-2))
+            (let [state (add-whats-necessary state [(- (- *abs-map-width* 16)) 0])]
+              (assoc state :cur-map simple-map-2)))
         (< y 0)
           (do
-            (add-whats-necessary [0 (- *abs-map-width* 16)]) 
-            (assoc state :cur-map simple-map-2))
+            (let [state (add-whats-necessary state [0 (- *abs-map-width* 16)])]
+              (assoc state :cur-map simple-map-2)))
         (> (+ y *player-width*) *abs-map-width*)
           (do
-            (add-whats-necessary [0 (- (- *abs-map-width* 16))]) 
-            (assoc state :cur-map simple-map-2))
+            (let [state (add-whats-necessary state [0 (- (- *abs-map-width* 16))])]
+              (assoc state :cur-map simple-map-2)))
         :else state)))
+
+(defn pos-from-state [state]
+  (list (get state :x) (get state :y)))
 
 (defn move-char [keys-down state]
   "Moves the character if necessary. Returns the game state. Does not worry about going offscreen.
   Does not actually change game state at this point, although in the future it should (w/r/t x, y)."
 
   (let [[dx dy] (get-delta keys-down)
-        char-pos [@x @y]
         vect-x  [dx  0]
-        vect-y  [0  dy]]
-    (when (valid-position? (+list vect-x char-pos))
-        (add-whats-necessary vect-x))
-    (let [char-pos [@x @y]] ;need to rebind because x may have changed.
-      (when (valid-position? (+list vect-y char-pos))
-        (add-whats-necessary vect-y)))
+        vect-y  [0  dy]
+        
+        state (if (valid-position? (+list vect-x (pos-from-state state)))
+                (add-whats-necessary state vect-x)
+                state)
+        state (if (valid-position? (+list vect-y (pos-from-state state)))
+                (add-whats-necessary state vect-y)
+                state)]
     state))
 
 (defn game-step [keys-down state]
@@ -303,37 +310,36 @@
        (> y (- *tile-width*))
        (< y *abs-map-width*)))
 
-(defn draw-tile [gfx x y type]
+(defn draw-tile [state gfx x y type]
   "Draws an individual tile."
-  (let [[tile-x tile-y] (rel-to-pos x y)]
+  (let [[tile-x tile-y] (rel-to-pos state x y)]
     (when (visible-abs? tile-x tile-y)
       (draw-abs gfx tile-x tile-y type))))
 
-
-(defn draw-tiles [gfx]
+(defn draw-tiles [state gfx]
   "Draw all tiles."
   (doseq [x (range *map-width*)
           y (range *map-width*)]
-    (draw-tile gfx x y (get-tile-type (get-tile-data x y)))))
+    (draw-tile state gfx x y (get-tile-type (get-tile-data x y)))))
 
-(defn draw-characters [gfx window]
+(defn draw-characters [state gfx window]
   "Draw all characters."
   (doseq [key (keys *characters*)]
-    (let [[x y] (abs-to-abs @x @y)]
+    (let [[x y] (abs-to-abs state (get state :x) (get state :y))] ;TODO: indicates easier way
       (draw-abs gfx x y (get-tile-type \c)))))
 
-(defn draw-state [gfx window]
+(defn draw-state [state gfx window]
   "The top level drawing function."
   (let [image (.createImage window *width* *height*)
         off-gfx (.createGraphics image)]
-    (draw-tiles off-gfx)
-    (draw-characters off-gfx window)
+    (draw-tiles state off-gfx)
+    (draw-characters state off-gfx window)
     (.drawImage gfx image 0 0 window)))
 
 (defn game-loop [frame state]
   (let [gfx (.getGraphics frame)]
     (Thread/sleep *delay*)
-    (draw-state gfx frame)
+    (draw-state state gfx frame)
     (let [new-state (game-step (get-keys-down keys-down) state)]
       (recur frame new-state))))
 
@@ -352,6 +358,6 @@
 (defn main []
   ;Switch do with future to run game-loop in a separate thread. This makes the repl work. Neat!
   (do (game-loop (make-frame *width* *height*)
-                 (hash-map :cur-map simple-map))))
+                 (hash-map :cur-map simple-map :x 30 :y 30))))
 
 (main)
